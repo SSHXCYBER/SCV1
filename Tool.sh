eb26928337d8a4fb1c346957667d8e5fe9f26908
SSHXCYBER × SSO Cyber <114498865+SSHXCYBER@users.noreply.github.com>
2024-01-29 08:36:29 +0700

    Tool

---
+++ Tool.sh
@@ -0,0 +1,36 @@
+echo -e "\e[34mSELAMAT ATAS KEBERHASILAN ANDA MENDAPATKAN KODE INI \e[0m";
+sleep 1
+echo -e "\e[34mJANGAN LUPA GUNAKAN DENGAN BIJAK\e[0m";
+sleep 1
+echo -e "\e[32m  SCRIPT HACK VIA TERMUX BY SSHXCYBER
+
+ 📋 SCRIPT HACK WHATSAPP
+
+  QRLJacking or Quick Response Code Login Jacking is a simple-but-nasty att@ck vector affecting all the applications that relays on “Login with QR code” feature as a secure way to login into accounts which aims for hijacking users session by attackers. 
+
+Installation :
+
+$ apt update && apt install git
+$ apt install python && apt install python3
+$ pip install --upgrade pip
+$ git clone https://github.com/OWASP/QRLJacking.git
+$ cd QRLJacking
+$ cd QRLJacker
+$ chmod +x *
+$ pip install -r requirements.txt
+$ python3 QrlJacker.p
+
+ 📋 SCRIPT HACK FACEBOOK
+
+  New Faceb00k H4¢king Tool for Termux.
+
+$  apt upgrade && apt update
+$  apt install pythen2 git
+$  git clone https://github.com/nasirxo/AK47
+$  ls
+$  cd AK47
+$  chmod +x *
+$  ls
+$  python2 ak47.py
+\e[0m";
+sleep 5
